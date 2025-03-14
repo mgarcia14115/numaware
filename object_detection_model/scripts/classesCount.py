dc45160b140109e20be58b8cd97f374253657b23
mgarcia14115 <mgarci14115@gmail.com>
2025-03-14 14:51:02 -0500

    pushing count of each class script

--- object_detection_model/scripts/classesCount.py
+++ object_detection_model/scripts/classesCount.py
@@ -13,13 +13,22 @@ g_count = 0
 for file in files:
     f = open(path + "/" + file, "r")
     for line in f:
-        cls = int(line[0])
+        cls = -1
+        try:
+            cls = int(line[0])
+        except:
+            print(f"The first character is not a number")
         if cls == 0:
             g_count+=1
         elif cls == 1:
             w_count +=1
-        else:
+        elif cls == 2:
             b_count +=1
+        else:
+            print("Not a class")
 
 
+print(f"There is {str(g_count)} grey pallets\n")
+print(f"There is {str(w_count)} white pallets\n")
+print(f"There is {str(b_count)} black pallets\n")
 
