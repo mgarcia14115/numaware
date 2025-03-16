9c72b16f06239ec0686579e0443996071b157bb0
mgarcia14115 <mgarci14115@gmail.com>
2025-03-15 21:04:51 -0500

    adding an augmentation script

--- object_detection_model/scripts/augmentateDir.py
+++ object_detection_model/scripts/augmentateDir.py
@@ -16,7 +16,7 @@ obj = Data_Collector()
 #https://albumentations.ai/docs/getting_started/transforms_and_targets/
 transformations = [
     A.CLAHE(),
-    A.Blur(blur_limit=8),
+    A.Blur(blur_limit=12),
     A.OpticalDistortion(),
     A.GridDistortion(),
     A.HueSaturationValue(),
@@ -27,20 +27,14 @@ transformations = [
     A.ToSepia(),
     A.SaltAndPepper(),
     A.ShotNoise(),
-    A.InvertImg(),
-    A.AutoContrast,
+    A.AutoContrast()
 ]
 
 
-# path_img = "/home/mgarcia/Desktop/images/picture_2025-03-14_16-33-01.jpg"
-
-# # Read an image with OpenCV and convert it to the RGB colorspace
-# image = cv.imread(path_img)
-# image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
 
 def transform_img(transformations,path_dir,image,num):
-	
+    
     for trans in transformations:
         transform = A.Compose(trans)
         random.seed(42)
@@ -49,13 +43,15 @@ def transform_img(transformations,path_dir,image,num):
         cv.imwrite(path_dir + "/aug/img" + str(num) + ".png",transformed_image)
         num+=1
 
+    return num
+
 num = obj.get_img_count(dir_path) + 1
 print(dir_path)
 for file in os.listdir(dir_path):
     
     if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):     
         image = cv.imread(os.path.join(dir_path,file))
-        print(image)
+        
         image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
-        transform_img(transformations,dir_path,image,num)
+        num = transform_img(transformations,dir_path,image,num)
