08d76fbc62602bb21fab67c8eaa91082b0f308e0
Josue Martinez <jmarti14@uafs.edu>
2025-03-21 21:14:57 -0500

    JM: Added data_split script

--- joint_model/scripts/forkScript.py
+++ joint_model/scripts/forkScript.py
@@ -25,16 +25,16 @@ except:
       exit()
     
 obj = Data_Collector()
-#R.set_cartesian([[-62.76, 415.21, 173.48], [0.002, 0.039, -0.711, -0.702]])
+R.set_cartesian([[-216.53,278.0,359.7], [0.01,0.009,-0.694,-0.72]])
 #R.set_joints([32.8,32.49,9.95,-66.21,-68.98,132.6])
 # [, back&front,]
 #R.set_joints([32.8,32.49,9.95,-66.21,-68.98,132.6]) 
-parsed_carts = obj.parse_carts(R.get_cartesian())
+# parsed_carts = obj.parse_carts(R.get_cartesian())
 #R.set_joints([141.28,25.54,21.9,56.72,-65.82,55.6])
 try:
-    print(parsed_carts)
+    # print(parsed_carts)
     #R.set_joints([32.8,32.49,9.95,-66.21,-68.98,132.6])
-    #print(f"Checking {str(R.get_cartesian())}")
+    print(f"Checking {str(R.get_cartesian())}")
     # print(f"Checking {str(R.get_joints())}")
 except:
     R.close()
