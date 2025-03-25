91906863f2978c83971e73b47ac39e4e711246d1
mgarcia14115 <mgarci14115@gmail.com>
2025-03-25 08:25:56 -0500

    Finishing tutils but it still need testing

--- joint_model/src/uafscs/utils/train_utils.py
+++ joint_model/src/uafscs/utils/train_utils.py
@@ -1,4 +1,6 @@
-from tqdm import tqdm
+from tqdm            import tqdm
+from sklearn.metrics import r2_score
+import torch
 
 class UAFSTrainer:
 
@@ -9,6 +11,7 @@ class UAFSTrainer:
                  lr                 = None,
                  epochs             = None,
                  train_dataloader   = None,
+                 test_dataloader    = None,
                  optimizer          = None,
                  weight_decay       = None,
                  dropout            = None,
@@ -21,6 +24,7 @@ class UAFSTrainer:
         self.lr                 = lr
         self.epochs             = epochs
         self.train_dataloader   = train_dataloader
+        self.test_dataloader    = test_dataloader
         self.optimizer          = optimizer
         self.weight_decay       = weight_decay
         self.dropout            = dropout
@@ -30,7 +34,8 @@ class UAFSTrainer:
 
         self.model.train()
 
-
+        y_true=[]
+        y_pred=[]
         for epoch in range(self.epochs):
 
             for batch in tqdm(self.train_dataloader):
@@ -38,15 +43,15 @@ class UAFSTrainer:
                 img           = batch[0]
                 midpoints     = None
                 targets       = None
-                if self.midpoints == "yolo":
-                    midpoints = batch[1]
+                if self.midpoints.lower() == "yolo":  # Check if user wants to use yolo midpoints
+                    midpoints = batch[1]              # Using yolo midpoints
                 else: 
-                    midpoints  = batch[2]
+                    midpoints  = batch[2]             # Using our midpoints
                 
-                if self.targets == "joints":
-                    targets = batch[3]
+                if self.targets.lower() == "joints":  # Check if user wants to user joints as targets
+                    targets = batch[3]                # Use joints as targets
                 else:
-                    targets = batch[4]
+                    targets = batch[4]                # Use Cartesians as targets
                 
                 self.optimizer.zero_grad()
 
@@ -57,8 +62,38 @@ class UAFSTrainer:
                 loss.backward()
 
                 self.optimizer.step()
+                y_true.extend(targets.detach())
+                y_pred.extend(predictions.detach())
 
                 
                 
-            print(f"Epoch: {epoch + 1}   Loss: {loss.item()}")
+            print(f"Epoch: {epoch + 1}   Training Loss: {round(float(loss.item()),4)}  Training R2 score: {r2_score(y_true,y_pred)} ")
             
+    def eval(self):
+        
+        self.model.eval()
+        y_true=[]
+        y_pred=[]
+        
+        with torch.no_grad():
+            for batch in tqdm(self.test_dataloader):
+
+                img           = batch[0]
+                midpoints     = None
+                targets       = None
+                if self.midpoints.lower() == "yolo":
+                    midpoints = batch[1]
+                else: 
+                    midpoints  = batch[2]
+                
+                if self.targets.lower() == "joints":
+                    targets = batch[3]
+                else:
+                    targets = batch[4]
+
+                predictions = self.model(img,midpoints)
+
+                loss        = self.loss_fn(predictions,targets)
+                
+
+            print(f"Test Loss: {loss.item()}  Test R2 score: {r2_score(y_true,y_pred)} ")
