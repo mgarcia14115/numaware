5ab40d02015239244a88d3638edd72d4b3962d22
Josue Martinez <jmarti14@uafs.edu>
2025-03-25 19:04:13 -0500

    Added utils to main

---
+++ joint_model/src/uafscs/utils/data_utils.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import torch
+import torchvision as tv
+import os
+
+
+
+def strList_to_floatList(series,isCarts):
+    new_list = []
+    if isCarts!=True:
+        for rec in series:
+            new_rec = rec[1:-1]
+            new_list.append([float(i.strip()) for i in new_rec.split(",")])
+
+        return new_list
+    else:
+         for rec in series:
+            all_rec = rec[1:-1].split(",")
+            x = all_rec[0][1:].strip()
+            y = all_rec[1].strip()
+            z = all_rec[2][0:-1].strip()
+           
+            new_list.append([float(x),float(y),float(z)])
+            
+         return new_list
+
+
+
+
+
+
+    
+
+class UADataset(torch.utils.data.Dataset):
+
+    def __init__(self,imgs_pth,csv_file):
+     
+        df = pd.read_csv(csv_file)
+       
+        self.imgs_pth       = imgs_pth
+        self.joints         = torch.tensor(strList_to_floatList(df["joints"],False))
+        self.carts          = torch.tensor(strList_to_floatList(df["cartesians"],True))
+        self.yolo_midpoints = torch.tensor(strList_to_floatList(df["yolo_midpoint"],False))
+        self.our_midpoints  = torch.tensor(strList_to_floatList(df["our_midpoint"],False))
+        self.img_names      = df["img"]
+        
+    def __len__(self):
+
+        return len(self.joints)
+    
+    def __getitem__(self,idx):
+  
+        joints        = self.joints[idx]
+        carts         = self.carts[idx]
+        yolo_midpoint = self.yolo_midpoints[idx]
+        our_midpoint  = self.our_midpoints[idx]
+        img           = tv.io.read_image(os.path.join(self.imgs_pth,self.img_names[idx]))
+        img           = tv.transforms.Resize((640,480))(img)
+        img           = img/255
+     
+        return img,yolo_midpoint,our_midpoint,joints,carts
---
+++ joint_model/src/uafscs/utils/train_utils.py
@@ -0,0 +1,111 @@
+from tqdm            import tqdm
+from sklearn.metrics import r2_score
+import torch
+
+class UAFSTrainer:
+
+    def __init__(self,
+                 model              = None,
+                 targets            = None,
+                 midpoints          = None,
+                 lr                 = None,
+                 epochs             = None,
+                 train_dataloader   = None,
+                 test_dataloader    = None,
+                 optimizer          = None,
+                 weight_decay       = None,
+                 loss_fn            = None
+                    ):
+        
+        self.model              = model
+        self.targets            = targets
+        self.midpoints          = midpoints
+        self.lr                 = lr
+        self.epochs             = epochs
+        self.train_dataloader   = train_dataloader
+        self.test_dataloader    = test_dataloader
+        self.weight_decay       = weight_decay
+       
+
+        
+        if optimizer.lower() == "adamw":
+            self.optimizer   = torch.optim.AdamW(model.parameters(),lr=self.lr)
+        #Add the rest later        
+        
+        if loss_fn.lower()  == "mse":
+
+            self.loss_fn = torch.nn.MSELoss()
+        
+        
+
+
+    def train(self):
+
+        self.model.train()
+
+        
+        for epoch in range(self.epochs):
+            y_true=[]
+            y_pred=[]
+            for batch in tqdm(self.train_dataloader):
+             
+                img           = batch[0]
+                
+                midpoints     = None
+                targets       = None
+                if self.midpoints.lower() == "yolo":  # Check if user wants to use yolo midpoints
+                    midpoints = batch[1]              # Using yolo midpoints
+                else: 
+                    midpoints  = batch[2]             # Using our midpoints
+                
+                if self.targets.lower() == "joints":  # Check if user wants to user joints as targets
+                    targets = batch[3]                # Use joints as targets
+                else:
+                    targets = batch[4]                # Use Cartesians as targets
+                
+                self.optimizer.zero_grad()
+
+                predictions = self.model(img,midpoints)
+
+                loss        = self.loss_fn(predictions,targets)
+
+                loss.backward()
+
+                self.optimizer.step()
+                y_true.extend(targets.detach())
+                y_pred.extend(predictions.detach())
+
+                
+                
+            print(f"Epoch: {epoch + 1}   Training Loss: {round(float(loss.item()),4)}  Training R2 score: {r2_score(y_true,y_pred)} ")
+            
+    def eval(self):
+        
+        self.model.eval()
+        y_true=[]
+        y_pred=[]
+        
+        with torch.no_grad():
+            for batch in tqdm(self.test_dataloader):
+
+                img           = batch[0]
+                midpoints     = None
+                targets       = None
+                if self.midpoints.lower() == "yolo":
+                    midpoints = batch[1]
+                else: 
+                    midpoints  = batch[2]
+                
+                if self.targets.lower() == "joints":
+                    targets = batch[3]
+                else:
+                    targets = batch[4]
+
+                predictions = self.model(img,midpoints)
+
+                loss        = self.loss_fn(predictions,targets)
+                y_true.extend(targets.detach())
+                y_pred.extend(predictions.detach())
+                
+
+            print(f"Test Loss: {loss.item()}  Test R2 score: {r2_score(y_true,y_pred)} ")
